18ecadeec4acb04943ea284d9f01a3c010791b64
Gajdos Tamás <gajdipajti@gmail.com>
2014-01-29 18:34:46 +0100

    minor

--- ice_dev.m
+++ ice_dev.m
@@ -1,13 +1,16 @@
 % Anisotropy scan
 % Author:   gajdost
-% Version:  0.0.1-dev
+% Version:  0.0.2-dev
 
 % Area selection %
 % In this part you can select a crop size
 % This is a simple split
+areaUP = [200 100 699 350];
+areaDW = [200 550 699 350];
 
 % An input file
-myHome     = '/home/freeman';
+myHome     = '/home/freeman/';
+% myHome   = '/home/gajdost/';
 myWorkDir  = 'munka/adoptim/2013_05_30_GFPAnisData/';
 myFileData = 'test1_a2_stack.tif';
 
@@ -16,19 +19,16 @@ myiInfo = imfinfo([myHome, myWorkDir, myFileData], 'tif');  % Extract file heade
 numberOfFrames = numel(myiInfo);                            % Number of images in the tif
 
 %for lpFrames = 1:numberOfFrames
-for lpFrames = 1:100
+iUPSum = zeros(351,700,100,'uint16');
+%%
+for lpFrames = 150:249
     % Work with small areas
-    iDat = imread([myHome, myWorkDir, myFileData], 'tif', 'Index', lpFrames, 'Info', myiInfo, 'CellArray', '{[200 899], [100 349]}');
+    iDat = imread([myHome, myWorkDir, myFileData], 'tif', 'Index', lpFrames, 'Info', myiInfo); % , 'CellArray', '{[200 899], [100 349]}'
     
     iUPDat = imcrop(iDat, areaUP);
-    iDWDat = imcrop(iDat, areaDW);
-    iUPSum(:,:,lpFrames) = iUPDat;
-    iUPRed = iUPDat - 1800;
-    iDWRed = iDWDat - 1400;
-    
-    sUPRed = sUPRed + iUPRed;
-    sDWRed = sDWRed + iDWRed;
-
-    %figure(9); hist(double(iUPDat(:)), [50:20:30000]);
-    %figure(10); hist(double(iDWDat(:)), 500);
+    iUPSum(:,:,lpFrames-149) = iUPDat;
 end
+%% Search Code
+% A code part where...
+
+
