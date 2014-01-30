768c60b3805232affb4bbc0bd0ec3ac9cb708057
Gajdos Tamás <gajdipajti@gmail.com>
2014-01-30 10:10:14 +0100

    Continue...

--- ice_dev.m
+++ ice_dev.m
@@ -17,18 +17,34 @@ myFileData = 'test1_a2_stack.tif';
 %Read in
 myiInfo = imfinfo([myHome, myWorkDir, myFileData], 'tif');  % Extract file headers and info
 numberOfFrames = numel(myiInfo);                            % Number of images in the tif
+%%
+iUPSum = zeros(351,700,200,'uint16');
 
 %for lpFrames = 1:numberOfFrames
-iUPSum = zeros(351,700,100,'uint16');
-%%
-for lpFrames = 150:249
+for lpFrames = 1:249
     % Work with small areas
     iDat = imread([myHome, myWorkDir, myFileData], 'tif', 'Index', lpFrames, 'Info', myiInfo); % , 'CellArray', '{[200 899], [100 349]}'
     
     iUPDat = imcrop(iDat, areaUP);
-    iUPSum(:,:,lpFrames-149) = iUPDat;
+    iUPSum(:,:,lpFrames) = iUPDat;
 end
 %% Search Code
-% A code part where...
-
-
+% A code part where the pixel marker works.
+mapUPSum = zeros(351,700,250,'uint8');
+mapUPCount = zeros(351,700,'uint8');
+for x = 1:700
+    for y = 1:351
+        for z = 1:249
+            if (z > 1)
+                if ((iUPSum(y,x,z) > 2000) && (mapUPSum(y,x,z-1) > 0))
+                    mapUPSum(y,x,z) = mapUPSum(y,x,z) + 1;
+                    mapUPCount(y,x) = mapUPCount(y,x) + 1;
+                end
+            end
+            if (iUPSum(y,x,z) > 2500)
+                % The first hit is not counted to the global.
+                mapUPSum(y,x,z) = mapUPSum(y,x,z) + 1;
+            end
+        end     
+    end
+end
