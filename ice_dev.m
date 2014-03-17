1c12c38e65016edca4d1c605544ff4a38b10a21c
Gajdos Tamás <gajdipajti@gmail.com>
2014-03-17 15:43:04 +0100

    Outsourced some pre-analysis code to a function.

--- ice_dev.m
+++ ice_dev.m
@@ -9,8 +9,8 @@ areaUP = [200 100 699 350];
 areaDW = [200 550 699 350];
 
 % An input file
-myHome     = '/home/freeman/';
-% myHome     = '/home/gajdost/';
+%myHome     = '/home/freeman/';
+myHome     = '/home/gajdost/';
 myWorkDir  = 'munka/adoptim/2013_05_30_GFPAnisData/';
 myFileData = 'test1_a2_stack.tif';
 
@@ -19,6 +19,7 @@ myiInfo = imfinfo([myHome, myWorkDir, myFileData], 'tif');  % Extract file heade
 numberOfFrames = numel(myiInfo);                            % Number of images in the tif
 %%
 iUPSum = zeros(351,700,200,'uint16');
+iDWSum = zeros(351,700,200,'uint16');
 zEnd=249;
 %zEnd=499;
 %for lpFrames = 1:numberOfFrames
@@ -28,135 +29,18 @@ for lpFrames = 1:zEnd
     
     iUPDat = imcrop(iDat, areaUP);
     iUPSum(:,:,lpFrames) = iUPDat;
+    iDWDat = imcrop(iDat, areaDW);
+    iDWSum(:,:,lpFrames) = iDWDat;
 end
 clear iDat;
 clear iUPDat;
+clear iDWDat;
 clear myiInfo;
 %% Search Code - pre-processer
 % A code part where the pixel marker works.
-mapUPSum = zeros(351,700,zEnd,'uint16');
-mapUPCount = zeros(351,700,'uint16');
-myZBegin=zeros('uint16');
-myZEnd=zeros('uint16');
-myZlimit=6;
-myZStack = zeros(1,4,'uint16');
-str=zeros(1,4,'uint16');
-% x=zeros('uint16'); % Not needed, and not working well
-% y=zeros('uint16');
-% z=zeros('uint16');
-for x = 1:700
-    for y = 1:351
-        mapZflag=0;
-        for z = 1:zEnd
-            if (iUPSum(y,x,z) > 2500)
-                % The first hit is not counted to the global.
-                mapUPSum(y,x,z) = mapUPSum(y,x,z) + 1;
-                if (mapZflag == 0)
-                    % First match
-                    mapZflag = 1;
-                    myZBegin=cast(z, 'uint16');
-                end
-            end
-            if (z > 1)
-                if ((iUPSum(y,x,z) > 2000) && (mapZflag > 0))
-                    mapUPSum(y,x,z) = mapUPSum(y,x,z) + 1;
-                    mapUPCount(y,x) = mapUPCount(y,x) + 1;
-                end
-                if ((mapZflag == 1) && (mapUPSum(y,x,z) < 1))
-                    % The end has been located
-                    mapZflag=0;
-                    myZEnd=cast(z, 'uint16');
-                    if ((myZEnd - myZBegin) > myZlimit)
-                        str = [ cast(y, 'uint16') cast(x, 'uint16') myZBegin myZEnd ];
-                        myZStack = [myZStack; str]; 
-                    end
-                end
-            end
-        end
-        if (mapZflag == 1)
-            %End of stack, must be the end of the blink also
-            myZEnd=cast(zEnd, 'uint16');
-            if ((myZEnd - myZBegin) > myZlimit)
-               str = [ cast(y, 'uint16') cast(x, 'uint16') myZBegin myZEnd ];
-               myZStack = [myZStack; str]; 
-            end
-        end
-    end
-end
-%% Max Value and fit
-% Implement me: Missing G calculations
-% http://www.mathworks.com/matlabcentral/answers/47428-to-find-the-maximum-value-in-a-matrix
-% http://www.mathworks.com/matlabcentral/newsreader/view_thread/269569
-mUPCount=mapUPCount;
-mFlag = 1;
-mLimit = 8;
-mUPSum = zeros(351,700,zEnd,'uint32');
-while mFlag >0
-    [mValue, idx] = max(mUPCount(:));
-    [mx, my] = ind2sub(size(mUPCount),idx); % Please note here the dimension chande mx<->mx
-    [ok, dx, dy, zb, ze] = iceCenter(mx, my, myZStack);
-    % Only run if iceCenter reported success.
-    if (ok > 0)
-        % Do a data plot from the full ZStack(R)
-        istack(:,1,1) = iUPSum(mx,my,:);
-        iceStackPlot(mx,my,istack);
-        % Generate indexes for sub
-        xb=mx-dx;
-        xe=mx+dx;
-        yb=my-dy;
-        ye=my+dy;
-        if zb < 2
-            % Must rework
-            bgs = 0;
-            tzb=zb;
-            tze=ze;
-        elseif zb < 3
-            bgs = 1;
-            tzb=zb-1;
-            tze=ze+1;
-        elseif ze > (zEnd-2)
-            % Needs reworking
-            bgs = 0;
-            tzb=zb;
-            tze=ze;
-        else
-            bgs = 2;
-            tzb=zb-2;
-            tze=ze+2;
-        end
-        if tze > zEnd
-            tze = zEnd;
-        end
-        % End generation
-        %
-        % Clear area, because iceCenter reported ok.
-        % Overlapping PSF warning!
-        for xi = xb:xe
-            for yi = yb:ye
-                mUPCount(xi,yi) = 0;
-            end
-        end
-        % Clear Complete
-        %
-        % Do the plot.
-        % Please implement more gaussian fit+volume calculators
-        mG = iceGaussian(bgs, iUPSum(xb:xe,yb:ye,tzb:tze) );
-        % Plot done
-        for mz = zb:ze
-            mUPSum(mx,my,mz) = mG(mz-(zb-1)); 
-        end
-    end
-    % Dump the max, it was no use.
-    mUPCount(mx,my)=0;
-    if mValue < mLimit
-        % Hurray limit reached!
-        mFlag = 0;
-    end
-    clear mG;
-end
-%clear iUPSum;
-%clear mapUPSum;
-clear mUPCount;
+[mUPSum, mUPXYZZ] = iceAnalysis(iUPSum,2500,2000,zEnd,6,8);
+[mDWSum, mDWXYZZ] = iceAnalysis(iDWSum,2500,2000,zEnd,6,8);
+% Two cubes are retured, which include the intensity data.
 %% Create a map
 mapFitData = zeros(351,700,'double');
 for cx = 1:700
